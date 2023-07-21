72c3d8e79428a59b940e6f40d899b7c0fc1e3b83
Aliceson Phillips <alicesonphillips@gmail.com>
2023-07-20 23:09:26 -0400

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,6 +1,7 @@
 const db = require('../../data/db-config.js');
 
 function find() {
+
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users.
@@ -18,6 +19,17 @@ function find() {
       }
     ]
    */
+
+    /*select
+      user_id,
+      username,
+      role_name
+    from users 
+      join roles on
+    users.role_id = roles.role_id;*/
+    return db('users')
+      .join('roles', 'users.role_id', 'roles.role_id')
+      .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
@@ -34,6 +46,21 @@ function findBy(filter) {
       }
     ]
    */
+  /*select
+      user_id,
+      username,
+      password,
+      role_name
+    from users 
+    join roles on
+      users.role_id = roles.role_id
+    where users.user_id = 1;
+ */
+    return db('users')
+      .join('roles', 'users.role_id', 'roles.role_id')
+      .select('user_id', 'username', 'password', 'role_name')
+      .where(filter)
+
 }
 
 function findById(user_id) {
